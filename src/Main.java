9d9cfce8fa881a22ca7a493dd122f047e476b92b
mariorfdez97 <UO296136@uniovi.es>
2026-02-16 16:31:09 +0100

    Changed Line 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Línea 1: Beggining of the program. (Modified by master)");
-        System.out.println("Línea 2: Changed from MAIN");
+        System.out.println("Line: Hello Master");
         System.out.println("Línea 3: End of the program. (Changed by working-branch)");
     }
 }
