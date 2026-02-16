297357a477c86731e5b1d2af3595ec7706d9236f
mariorfdez97 <UO296136@uniovi.es>
2026-02-16 14:01:56 +0100

    Initial code.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	
+    public static void main(String[] args) {
+        System.out.println("Línea 1: Beggining of the program.");
+        System.out.println("Línea 2: ---");
+        System.out.println("Línea 3: End of the program.");
+    }
+    
+}
